f566527ea38f1d722f186811fe161925f0b4648d
Tomasz Michalski <tomasz.a.michalski@gmail.com>
2023-05-05 20:59:07 +0200

    Tested on python 3.5 and it worked

--- img_aug.py
+++ img_aug.py
@@ -50,34 +50,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Processing <PIL.Image.Image image mode=RGB size=394x282 at 0x7F93A67D8CD0>: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 30/30 [00:00<00:00, 1445.75 Samples/s]
-# Processing <PIL.Image.Image image mode=RGB size=206x118 at 0x7F93AD1A48E0>:  50%|███████████████████████████████████████████████████████████████████                                                                   | 15/30 [00:00<00:00, 740.97 Samples/s]
-# Traceback (most recent call last):
-#   File "/home/robin/github/forked/ProtoPNet/img_aug.py", line 52, in <module>
-#     main()
-#   File "/home/robin/github/forked/ProtoPNet/img_aug.py", line 27, in main
-#     p.process()
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/Augmentor/Pipeline.py", line 391, in process
-#     self.sample(0, multi_threaded=True)
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/Augmentor/Pipeline.py", line 364, in sample
-#     for result in executor.map(self, augmentor_images):
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/concurrent/futures/_base.py", line 609, in result_iterator
-#     yield fs.pop().result()
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/concurrent/futures/_base.py", line 439, in result
-#     return self.__get_result()
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/concurrent/futures/_base.py", line 391, in __get_result
-#     raise self._exception
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/concurrent/futures/thread.py", line 58, in run
-#     result = self.fn(*self.args, **self.kwargs)
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/Augmentor/Pipeline.py", line 105, in __call__
-#     return self._execute(augmentor_image)
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/Augmentor/Pipeline.py", line 233, in _execute
-#     images = operation.perform_operation(images)
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/Augmentor/Operations.py", line 851, in perform_operation
-#     augmented_images.append(do(image))
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/Augmentor/Operations.py", line 843, in do
-#     image = image.crop((int(round(E)), int(round(A)), int(round(X - E)), int(round(Y - A))))
-#   File "/home/robin/anaconda3/envs/img_aug/lib/python3.9/site-packages/PIL/Image.py", line 1227, in crop
-#     raise ValueError(msg)
-# ValueError: Coordinate 'lower' is less than 'upper'
